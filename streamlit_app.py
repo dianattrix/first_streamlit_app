546c99f819a9b92057c4bcbee38ae36ed9d5828d
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2023-01-19 20:25:56 -0600

    Adding Snowflake connector and code

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_data_rows = my_cur.fetchall()
+streamlit.text("The Fruit Load List Contains:")
+streamlit.text(my_data_row)
